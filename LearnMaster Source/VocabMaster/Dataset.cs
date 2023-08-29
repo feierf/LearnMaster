dd2c5908cee9b906f87c29dcddee1831d99067e7
felix.feierabend <felix.feierabend@outlook.de>
2023-08-29 11:39:12 +0200

    implemented levenshtein-algorithm

--- LearnMaster Source/VocabMaster/Dataset.cs
+++ LearnMaster Source/VocabMaster/Dataset.cs
@@ -39,6 +39,37 @@ namespace LearnMaster
             _answeredIncorrect = false;
         }
 
+        private int CalculateLevenshteinDistance(string s1, string s2)
+        {
+            int n = s1.Length;
+            int m = s2.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            if (n == 0)
+                return m;
+
+            if (m == 0)
+                return n;
+
+            for (int i = 0; i <= n; d[i, 0] = i++) ;
+
+            for (int j = 0; j <= m; d[0, j] = j++) ;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    int cost;
+
+                    cost = s2[j - 1] == s1[i - 1] ? 0 : 1;
+
+                    d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
+                }
+            }
+
+            return d[n, m];
+        }
+
         public bool AnsweredCorrect(Dataset other)
         {
             string[] correctSolutions = this._solution.Split(';');
@@ -48,14 +79,16 @@ namespace LearnMaster
 
             foreach (string s in correctSolutions)
             {
-                if (solutionCorrect = other._solution.ToLower().Trim().Replace(" ", "") == s.ToLower().Trim().Replace(" ", ""))
+                if (solutionCorrect = other._solution.ToLower().Trim().Replace(" ", "") == s.ToLower().Trim().Replace(" ", "") 
+                    || CalculateLevenshteinDistance(other._solution.ToLower().Trim().Replace(" ", ""), s.ToLower().Trim().Replace(" ", "")) <= 2)
                 {
                     break;
                 }
             }
             foreach (string s in correctDescriptions)
             {
-                if (descriptionCorrect = other._description.ToLower().Trim().Replace(" ", "") == s.ToLower().Trim().Replace(" ", ""))
+                if (descriptionCorrect = other._description.ToLower().Trim().Replace(" ", "") == s.ToLower().Trim().Replace(" ", "")
+                    || CalculateLevenshteinDistance(other._description.ToLower().Trim().Replace(" ", ""), s.ToLower().Trim().Replace(" ", "")) <= 2)
                 {
                     break;
                 }
